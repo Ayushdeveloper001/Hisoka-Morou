06c9e4a5d0a95d167cbba5de0fb1ce90d9657280
Ayush <117559953+Ayushdeveloper001@users.noreply.github.com>
2023-01-15 14:08:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 /**
-   * Create By Dika Ardnt.
-   * Contact Me on wa.me/6288292024190
-   * Follow https://github.com/DikaArdnt
+   * Create By Ayush.
+   * Contact Me on wa.me/918602306506
+   * Follow https://github.com/ayushdeveloper001
 */
 
 const fs = require('fs')
@@ -18,9 +18,9 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','62895336722950','6287721317870','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
+global.owner = ['918602306506']
+global.premium = ['918602306506']
+global.packname = 'Ayush Developer'
 global.author = 'WhatsApp Bot'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
